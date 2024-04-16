8308723f3e61103d7749b6f3ee8effd2d65fa6e1
danaewgg <98764158+danaewgg@users.noreply.github.com>
2024-04-17 02:30:28 +0300

    Revision 1

--- Park.lua
+++ Park.lua
@@ -1,12 +1,13 @@
 if game.PlaceId == 8448881160 then
-    -- We'll see if this works, I guess
-    return game:GetService("ReplicatedStorage").Remotes.Teleport:InvokeServer("Park"), queue_on_teleport([[loadstring(request(Url = "https://raw.githubusercontent.com/danaewgg/ball-game/main/Park.lua").Body))()]])
+    game:GetService("ReplicatedStorage").Remotes.Teleport:InvokeServer("Park")
+    queue_on_teleport([[loadstring(request(Url = "https://raw.githubusercontent.com/danaewgg/ball-game/main/Park.lua").Body))()]])
+    return
 end
 
 local Players = game:GetService("Players")
 local HttpService = game:GetService("HttpService")
 
--- I've thought about caching the request arguments, but looking at my code above makes me think it won't make sense, it's already having to get game.PlaceId and I can't cache that so it'll slow down execution anyway
+-- I've thought about caching the request arguments, but looking at my code above makes me think it won't make sense, it's already having to get game.PlaceId, and I can't cache that, so it'll slow down execution anyway
 local function SendMessageToWebhook(message)
     local response = request({
         Url = "https://discord.com/api/webhooks/1229721351124942941/OciXX8P6Bky_yp4T9LHSaUClTOuhFVEDyGUHvfCQvrq2zYa8Ory-HepwWGKIn38o5KKy", -- I know
@@ -23,6 +24,6 @@ end)
 
 for _, player in next, Players:GetPlayers() do
     if player ~= Players.LocalPlayer then -- Faster than a guard clause with continue
-        SendMessageToWebhook(`block {player.UserId}`) -- Thought of using task.spawn, but it's just an unnecessary function call as I won't be checking the response after the request
+        SendMessageToWebhook(`block {player.UserId}`) -- Thought of using task.spawn, but it's just an unnecessary function call, as I won't be checking the response after the request
     end
 end
