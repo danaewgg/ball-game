f6d3c2f5acff33e3ac15fa959644721c10bf5519
danaewgg <98764158+danaewgg@users.noreply.github.com>
2024-04-17 13:48:42 +0300

    Revision 13

--- Park.lua
+++ Park.lua
@@ -1,10 +1,11 @@
 rconsoleprint("Checkpoint 1 - Script ran")
 
-if game.PlaceId == 8448881160 then
+local placeId = game.PlaceId
+if placeId == 8448881160 then
     rconsoleprint("Debug - Player is in plaza, teleporting to park")
     return game:GetService("ReplicatedStorage").Remotes.Teleport:InvokeServer("Park"), queue_on_teleport([[loadstring(request({Url = "https://raw.githubusercontent.com/danaewgg/ball-game/main/Park.lua"}).Body)()]])
 end
-
+if placeId ~= 10107441386 then return rconsoleprint("Debug - Returning script, the player wasn't in plaza or park") end
 rconsoleprint("Checkpoint 2 - Park matched, running code")
 
 local HttpService = game:GetService("HttpService")
@@ -12,34 +13,31 @@ local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local playersInServer = Players:GetPlayers()
 
-local URL = "https://discord.com/api/webhooks/1229721351124942941/OciXX8P6Bky_yp4T9LHSaUClTOuhFVEDyGUHvfCQvrq2zYa8Ory-HepwWGKIn38o5KKy" -- I know
-local METHOD = "POST"
-local HEADERS = {["Content-Type"] = "application/json"}
-local function SendMessageToWebhook(message)
-    request({
-        Url = URL,
-        Method = METHOD,
-        Body = HttpService:JSONEncode({content = message}),
-        Headers = HEADERS
-    })
-end
-
 LocalPlayer.OnTeleport:Connect(function(teleportState)
     rconsoleprint(`Debug - OnTeleport triggered with state "{teleportState}"`)
-    if teleportState ~= Enum.TeleportState.InProgress or #playersInServer <= 1 then return end -- '<=' is slightly faster than just '<' from my testing
+    if teleportState ~= Enum.TeleportState.InProgress or #playersInServer <= 1 then return end -- '<=' is faster than just '<' from my testing
 
     rconsoleprint("Debug - Queueing script execution")
     queue_on_teleport([[loadstring(request({Url = "https://raw.githubusercontent.com/danaewgg/ball-game/main/Park.lua"}).Body)()]])
     rconsoleprint("Debug - OnTeleport function finished running")
 end)
 
+if #playersInServer <= 1 then return rconsoleprint("Debug - Returning script, identified fresh park") end -- So we don't teleport back when we choose to leave ourselves
 rconsoleprint("Checkpoint 3 - Ready for main loop")
 
-if #playersInServer <= 1 then return end -- Stop the script if the park is fresh, so we don't teleport back when we choose to leave
-
+local URL = "https://discord.com/api/webhooks/1229721351124942941/OciXX8P6Bky_yp4T9LHSaUClTOuhFVEDyGUHvfCQvrq2zYa8Ory-HepwWGKIn38o5KKy" -- I know
+local METHOD = "POST"
+local HEADERS = {["Content-Type"] = "application/json"}
 for _, player in next, playersInServer do
     if player ~= LocalPlayer then -- Faster than a guard clause with continue
-        SendMessageToWebhook(`block {player.UserId}`) -- Thought of using task.spawn, but it's just an unnecessary function call, as I won't be checking the response after the request
+        task.spawn(function() -- Because request yields, I didn't know
+            request({
+                Url = URL,
+                Method = METHOD,
+                Body = HttpService:JSONEncode({content = `block {player.UserId}`}),
+                Headers = HEADERS
+            })
+        end)
     end
 end
 
